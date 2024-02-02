95025f7ec1706fad76c59c9ae52d4018e5e1d098
Rodrigo Carvalho Dev <rodrigosms103042r6@gmail.com>
2024-02-01 22:13:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+function link() {
+    const url_contact = "templates/contact.html";
+    setTimeout(() => {
+        window.open(url_contact, "_blank");
+    }, 500);
+}
+function link_company() {
+    const url_company = "templates/company.html";
+    setTimeout(() => {
+        window.open(url_company, "_blank");
+    }, 500);
+}
---
+++ index.ts
@@ -0,0 +1,17 @@
+function link(){
+  const url_contact = "templates/contact.html"
+  
+
+  setTimeout(() => {
+    window.open(url_contact,"_blank")
+  }, 500);
+}
+
+function link_company(){
+    const url_company = "templates/company.html"
+  
+
+    setTimeout(() => {
+      window.open(url_company,"_blank")
+    }, 500);
+}
